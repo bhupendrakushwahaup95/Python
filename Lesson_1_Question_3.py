7c89878c12dfc6024eaca3b7acc43f989bd6918b
bhupendrakushwahaup95 <bhupendrakushwahaup95@gmail.com>
2026-03-17 19:16:18 +0530

    Add files via upload
    Write a program to calculate Simple Interest.

---
+++ Lesson_1_Question_3.py
@@ -0,0 +1,6 @@
+p = float(input("Enter Principal: "))
+r = float(input("Enter Rate: "))
+t = float(input("Enter Time: "))
+# Logic: SI formula
+si = (p * r * t) / 100
+print("Simple Interest is:", si)
