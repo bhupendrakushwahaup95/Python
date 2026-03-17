82953f3712ae5f7ccdadf29a51129c06f44fe2fd
bhupendrakushwahaup95 <bhupendrakushwahaup95@gmail.com>
2026-03-17 19:13:55 +0530

    Add files via upload
    Write a program to calculate the sum of two numbers.

---
+++ Lesson_1_Question_1.py
@@ -0,0 +1,6 @@
+# Taking input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+# Addition logic
+result = num1 + num2
+print("The sum is:", result)
